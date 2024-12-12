79e292adb16378f26a40080aeec23bede69e88b5
yairgolo <yairgolo10@gmail.com>
2024-12-12 10:04:41 +0200

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");  // ייבוא הספרייה
 const fs = require("fs");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // שימוש ב-cors כדי לאפשר בקשות מכל דומיין
 app.use(express.json());
